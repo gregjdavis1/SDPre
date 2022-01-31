d0890fa526b6489b0c45499d00fbe60cd084faaa
Greg <gregjdavis1@gmail.com>
2022-01-30 21:46:44 -0500

    Objective3

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,11 @@
+public class Objective3Lab2 {
+  public static void main(String [] args){
+    //to find the hypotenuse for side 3:
+    double side1 = 10; //input size of side 1
+    double side2 = 8; //input size of side 2
+
+    double hypotenuse = Math.sqrt(side1*side1 + side2*side2);
+    System.out.println(hypotenuse);
+
+  }
+}
---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,14 @@
+public class Objective3Lab3 {
+  public static void main(String [] args){
+    int first = 1 + (int)(Math.random() * 6);
+    int second = 1 + (int)(Math.random() * 6);
+    int third = 1 + (int)(Math.random() * 6);
+    System.out.println("The following numbers were randomly chosen:");
+    System.out.println(first);
+    System.out.println(second);
+    System.out.println(third);
+    System.out.println("Here is their sum");
+    System.out.println(first + second + third);
+    System.out.println("I look forward to learning better line-formating skills.");
+  }
+}
